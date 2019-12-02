09dd9f962abf7c38fcefc1d6c4facd96e5c5fc4a
my9527 <2199048390@qq.com>
2019-12-02 16:39:05 +0800

    test
    test

--- locale/zh_CN.js
+++ locale/zh_CN.js
@@ -3,7 +3,7 @@
 
 _KC_PAGE_LANG_LOADER('zh_CN', {
   "login.or.siginup": "<a href='{loginUrl}'>Log In</a> or <a href='{registerUrl}'>Sign Up</a>",
-  "name": "Name",
+  "name": "Namesdfsdf",
   "i.know": "Okay, I Understand",
   "create.trade.code.title": "Create Trading Password",
   "create.trade.code.placeholder": "Create trading password (6 digit)",
