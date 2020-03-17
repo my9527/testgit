1b4204dd5f8524804ac73bb7134e542ce88df1be
my <yang.yang@kucoinc.com>
2020-03-17 18:27:42 +0800

    modified: 语言包更新

--- locale/zh_CN_temp.js
+++ locale/zh_CN_temp.js
@@ -1,7 +1,8 @@
 /* eslint-disable quotes,quote-props,max-len,no-multiple-empty-lines */
 _KC_PAGE_LANG_LOADER("en_US", {
   "login.or.siginup": "<a href='{loginUrl}'>Log In</a> or <a href='{registerUrl}'>Sign Up</a>",
-  "name": "Name",
+  "name": "ssss",
   "i.know": "Okay, I Understand",
   "margin.lend.confirm.sureLend": "Lend Confirmation",
+  "age": "aaaaa",
 });
