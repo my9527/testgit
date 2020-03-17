d8da6e6ebc337a159ee938f01ea769e8eac7f6d5
my <yang.yang@kucoinc.com>
2020-03-17 19:40:50 +0800

    sd

--- locale/zh_CN.js
+++ locale/zh_CN.js
@@ -1,16 +1,7 @@
 /* eslint-disable quotes,quote-props,max-len,no-multiple-empty-lines */
-<<<<<<< HEAD
 _KC_PAGE_LANG_LOADER("en_US", {
   "login.or.siginup": "<a href='{loginUrl}'>Log In</a> or <a href='{registerUrl}'>Sign Up</a>",
   "name": "Name",
   "i.know": "Okay, I Understand",
   "margin.lend.confirm.sureLend": "Lend Confirmation",
 });
-=======
-
-_KC_PAGE_LANG_LOADER('zh_CN', {
-  "name": "姓名",
-  "age": "345352"
-});
-
->>>>>>> 891b0b9a8b0d2e0548a0f938a71d1161fa5c3fb6
