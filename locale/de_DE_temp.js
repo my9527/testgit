de69301ce96d03f543dabdfd51a59c61aebe79d3
my <yang.yang@kucoinc.com>
2020-03-18 22:50:31 +0800

    modified: 语言包更新

--- locale/de_DE_temp.js
+++ locale/de_DE_temp.js
@@ -4,7 +4,7 @@
 _KC_PAGE_LANG_LOADER('de_DE', {
   "margin.lend.confirm.sureLend": "确认借出",
 
-  "global.slogan": "aaaaaa",
+  "global.slogan": "<a href=\\${valSig}https://www.baidu.com\\${valSig}> sd'dfs\\${valSig}sdf\\${valSig}dsfgsdfgsdfgf</a>",
   "age": "asfasdf",
 
   "global.slogan": "<a href=\"https://www.baidu.com\"> sd\'dfs\"sdf\"dsfgsdfgsdfgf</a>",
